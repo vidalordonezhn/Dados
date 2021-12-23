a3658fb13635edf729f752b3446acb9893c7060c
DESA <DESARROLLADOR@CNQRH12.azucareralagrecia.com>
2021-12-23 11:43:43 -0600

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dados';
+
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice4.png';
+  numero1: number = 1;
+  numero2: number = 2;
+
+  tirarDados():void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dadoIzquierda = '../assets/img/dice' + this.numero1 + '.png';
+    this.dadoDerecha = '../assets/img/dice' + this.numero2 + '.png';
+  }
 }
